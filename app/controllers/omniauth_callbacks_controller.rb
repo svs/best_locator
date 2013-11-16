a1ccc8141dfae5308ac4ac4da1b96875a1c18d3b
svs <svs@svs.io>
2013-11-16 16:42:18 +0530

    Signed in succesfully flash

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,7 +3,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
-      sign_in_and_redirect user, notice: "Signed in!"
+      flash[:notice] = "Signed in successfully"
+      sign_in_and_redirect user
     else
       session["devise.user_attributes"] = user.attributes
       redirect_to new_user_registration_url
