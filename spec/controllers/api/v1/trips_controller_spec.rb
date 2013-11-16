6603c0122fb4826719f4be5dbb82603f3c720976
svs <svs@svs.io>
2013-11-16 16:24:06 +0530

    refactored trips controller specs

--- spec/controllers/api/v1/trips_controller_spec.rb
+++ spec/controllers/api/v1/trips_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Api::V1::TripsController do
 
   let!(:user) { FactoryGirl.create(:user) }
-
+  let(:valid_params) { {"trip" => {}, :format => "json"}}
   context "unauthenticated" do
 
     it "should 302" do
@@ -28,15 +28,15 @@ describe Api::V1::TripsController do
 
 
   context "API token" do
-    let(:valid_params) { {"trip" => {}, :format => "json"}.merge(:user_email => user.email, :user_token => user.auth_token) }
+    let(:valid_api_params) { valid_params.merge(:user_email => user.email, :user_token => user.auth_token) }
     it "should 200" do
-      post(:create, valid_params)
+      post(:create, valid_api_params)
       response.status.should == 200
     end
 
     it "should create a trip" do
       expect {
-        post(:create, valid_params)
+        post(:create, valid_api_params)
       }.to change(Trip, :count).by(1)
     end
   end
