1adfbb8d15f2c82aca809c86df30a5aba7b5347e
svs <svs@svs.io>
2013-12-22 20:11:58 +0530

    made facebook login respond to json

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,8 +3,16 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
-      flash[:notice] = "Signed in successfully"
-      sign_in_and_redirect user
+      respond_to do |format|
+        format.html {
+          flash[:notice] = "Signed in successfully"
+          sign_in_and_redirect user
+        }
+        format.json {
+          sign_in user
+          render json: user
+        }
+      end
     else
       session["devise.user_attributes"] = user.attributes
       redirect_to new_user_registration_url
