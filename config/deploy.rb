8c3b745457b66a4de00550032ec97dc4d8d4b0ba
svs <svs@svs.io>
2013-12-27 14:48:05 +0530

    dealing with binstubs

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :log_level, :info
 # set :pty, true
 
 set :linked_files, %w{config/database.yml config/application.yml config/puma.rb}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :bundle_gemfile, -> { release_path.join('Gemfile') }
 set :bundle_dir, -> { shared_path.join('bundle') }
